9a11269782daca407e0cdbe0485147d9c81404e3
Shawn <shawntennity@gmail.com>
2019-11-29 17:26:18 -0500

    Created components, actions, and reducers folders.  Added redux, react-redux, and redux-thunk.  Created store in index.js.  Created dummy reducer.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducers from './reducers';
+import reduxThunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+)
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const users = () => []
+
+export default combineReducers({
+  users
+})
