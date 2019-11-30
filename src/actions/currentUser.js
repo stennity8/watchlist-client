824e66cb2fdea255f7f465511376375cb3b753a4
Shawn <shawntennity@gmail.com>
2019-11-30 17:22:57 -0500

    Refactored login and getCurrentUser actions to utilize async/await.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -26,45 +26,20 @@ export const login = (credentials) => async dispatch => {
   }
 }
 
-// export const login = (credentials) => {
-//   return dispatch => {
-//     return fetch('http://localhost:3001/api/v1/login', {
-//       credentials: "include",
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify(credentials)
-//     })
-//       .then(response => response.json())
-//       .then(user => {
-//         if (user.error) {
-//           alert(user.error)
-//         } else {
-//           dispatch(setCurrentUser(user))
-//         }
-//       }
-//       )
-//   }
-// }
+export const getCurrentUser = () => async dispatch => {
+  const response = await fetch('http://localhost:3001/api/v1/current_user', {
+    credentials: "include",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
 
-export const getCurrentUser = () => {
-  return dispatch => {
-    return fetch('http://localhost:3001/api/v1/current_user', {
-      credentials: "include",
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(response => response.json())
-      .then(user => {
-        if (user.error) {
-          console.log(user.error)
-        } else {
-          dispatch(setCurrentUser(user))
-        }
-      }
-      )
+  const user = await response.json()
+
+  if (user.error) {
+    alert(user.error)
+  } else {
+    dispatch(setCurrentUser(user))
   }
 }
