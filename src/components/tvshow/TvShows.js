c80958526f656ba156be1d06f60e29efd753207e
Shawn <shawntennity@gmail.com>
2019-12-01 18:26:19 -0500

    Added button to popular tv shows for adding to watchlist when user is logged in.

--- src/components/tvshow/TvShows.js
+++ src/components/tvshow/TvShows.js
@@ -1,25 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const renderTvShow = ({ name, overview, vote_average, vote_count, first_air_date, id, poster_path }) => {
-  return (
-    <div className="ui grid item" key={id}>
-      <div className="four wide column">
-        <img className="ui middle aligned small image" src={`https://image.tmdb.org/t/p/w500${poster_path}`} />
-      </div>
-      <div className="content twelve wide column">
-        <h2 className="header">{name}</h2>
-        <p>{overview}</p>
-        <div className="row">
-          <p className="column"><strong>Voter Score: </strong>{`${vote_average} based on ${vote_count} votes`}</p>
-          <p className="column"><strong>First Aired: </strong>{first_air_date}</p>
+const TvShows = ({ shows, currentUser }) => {
+
+  const renderTvShow = ({ name, overview, vote_average, vote_count, first_air_date, id, poster_path }) => {
+    return (
+      <div className="ui grid item" key={id}>
+        <div className="four wide column">
+          <img className="ui middle aligned small image" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="movie poster" />
+        </div>
+        <div className="content twelve wide column">
+          <h2 className="header">{name}</h2>
+          <p>{overview}</p>
+          <div className="row">
+            <p className="column"><strong>Voter Score: </strong>{`${vote_average} based on ${vote_count} votes`}</p>
+            <p className="column"><strong>First Aired: </strong>{first_air_date}</p>
+            {renderWatchListButton()}
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
-const TvShows = ({ shows }) => {
+  const renderWatchListButton = () => {
+    if (currentUser) {
+      return (
+        <button className="column ui button green">
+          <i className="youtube icon"></i>
+          Add to WatchList
+        </button>
+      )
+    }
+  }
+
   return (
     <div className="ui celled list">
       {shows.map(renderTvShow)}
@@ -28,7 +41,10 @@ const TvShows = ({ shows }) => {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  return { shows: state[ownProps.reduxShowType] }
+  return {
+    shows: state[ownProps.reduxShowType],
+    currentUser: state.currentUser
+  }
 }
 
 export default connect(mapStateToProps)(TvShows)
